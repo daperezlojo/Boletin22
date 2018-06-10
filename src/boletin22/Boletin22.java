88bdc7814976c8b6cfd299ab2b76876e2fedcb1e
daperezlojo <32535705+daperezlojo@users.noreply.github.com>
2018-06-10 13:25:51 +0200

    .

---
+++ src/boletin22/Boletin22.java
@@ -0,0 +1,12 @@
+package boletin22;
+
+import boletin22.Ventana2;
+
+public class Boletin22 {
+
+    public static void main(String[] args) {
+            Ventana1 V = new Ventana1();
+            V.setVisible(true);
+    }
+    
+}
